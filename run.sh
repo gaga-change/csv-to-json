13df4af0d1bb9a03f8e45ad0cae24ef43b0c6518
严俊东 <1172482914@qq.com>
2020-09-25 08:57:54 +0800

    运行脚本

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+project_path=$(cd `dirname $0`; pwd)
+project_name="${project_path##*/}"
+git branch
+git pull
+docker run -it --rm --name ${project_name}-install  -v ${project_path}:/root/app -w /root/app node:10-alpine npm i
+docker stop ${project_name}
+docker run -it --rm -d --name ${project_name} -p 8720:3000 -v ${project_path}:/root/app -w /root/app node:10-alpine npm run start
